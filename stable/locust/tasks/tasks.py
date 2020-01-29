694d7a48a5f5dae67a3b8fcf84dca7ac7952a9cd
David Heereman <david.heereman@jambit.com>
2020-01-29 13:42:38 +0100

    Add tasks for petclininc

--- stable/locust/tasks/tasks.py
+++ stable/locust/tasks/tasks.py
@@ -2,8 +2,17 @@ from locust import HttpLocust, TaskSet, task
 
 class ElbTasks(TaskSet):
   @task
-  def status(self):
-      self.client.get("/status")
+  def getHome(self):
+      self.client.get("/")
+  @task
+  def getOwners(self):
+      self.client.get("/owners/")
+  @task
+  def getVets(self):
+      self.client.get("/vets/")
+  @task
+  def findOwners(self):
+      self.client.get("/owners/find/")
 
 class ElbWarmer(HttpLocust):
   task_set = ElbTasks
